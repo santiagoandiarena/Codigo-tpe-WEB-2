c20b603e8f7500dfedccda25edb7b92d0fe01ec5
fabian <fabian.romero.ignacio@gmail.com>
2024-10-16 15:22:21 -0300

    va sin imagenes

--- g84_db_tiendaropa.sql
+++ g84_db_tiendaropa.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-10-2024 a las 02:33:18
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Tiempo de generación: 16-10-2024 a las 20:18:20
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,61 +32,19 @@ CREATE TABLE `articulo` (
   `nombre` varchar(80) NOT NULL,
   `valor` int(11) NOT NULL,
   `descripcion` varchar(100) NOT NULL,
-  `ID_categoria` int(11) NOT NULL
+  `ID_categoria` int(11) NOT NULL,
+  `Imagen` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `articulo`
 --
 
-INSERT INTO `articulo` (`ID_articulo`, `nombre`, `valor`, `descripcion`, `ID_categoria`) VALUES
-(2, 'Remera gris', 15000, 'Remera color gris manga larga  ', 1),
-(3, 'Buzo negro largo', 30000, 'Buzo color negro oversize hombre-mujer', 4),
-(4, 'Pantalon rojo', 20000, 'Pantalon largo color rojo con detalles azules', 2),
-(5, 'Campera con capucha', 40000, 'Campera tipo rompevientos con capucha para invierno', 3);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `categoria`
---
-
-CREATE TABLE `categoria` (
-  `ID_categoria` int(11) NOT NULL,
-  `nombre` varchar(80) NOT NULL,
-  `genero` varchar(10) NOT NULL,
-  `temporada` varchar(40) NOT NULL,
-  `marca` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `categoria`
---
-
-INSERT INTO `categoria` (`ID_categoria`, `nombre`, `genero`, `temporada`, `marca`) VALUES
-(1, 'Remera', 'Masculino', 'Verano', 'Nike'),
-(2, 'Pantalon', 'Femenino', 'Invierno', 'Adidas'),
-(3, 'Campera', 'Masculino', 'Invierno', 'Puma'),
-(4, 'Buzo', 'Masculino', 'Invierno', 'Nike');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
-
-CREATE TABLE `usuario` (
-  `ID_usuario` int(11) NOT NULL,
-  `nombreUsuario` varchar(200) NOT NULL,
-  `contraseña` varchar(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `usuario`
---
-
-INSERT INTO `usuario` (`ID_usuario`, `nombreUsuario`, `contraseña`) VALUES
-(1, 'webadmin', '$2y$10$7NwzlSp0YB06/KYhmeV1zeNyqJiB8RCAd7zmnRTvlaOTHsZgJ64YK');
+INSERT INTO `articulo` (`ID_articulo`, `nombre`, `valor`, `descripcion`, `ID_categoria`, `Imagen`) VALUES
+(2, 'Remera gris', 15000, 'Remera color gris manga larga  ', 1, ''),
+(3, 'Buzo negro largo', 30000, 'Buzo color negro oversize hombre-mujer', 4, ''),
+(4, 'Pantalon rojo', 20000, 'Pantalon largo color rojo con detalles azules', 2, ''),
+(5, 'Campera con capucha', 40000, 'Campera tipo rompevientos con capucha para invierno', 3, '');
 
 --
 -- Índices para tablas volcadas
@@ -100,18 +58,6 @@ ALTER TABLE `articulo`
   ADD KEY `ID_categoria` (`ID_categoria`);
 
 --
--- Indices de la tabla `categoria`
---
-ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`ID_categoria`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`ID_usuario`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -120,18 +66,6 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `articulo`
   MODIFY `ID_articulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
-
---
--- AUTO_INCREMENT de la tabla `categoria`
---
-ALTER TABLE `categoria`
-  MODIFY `ID_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
-
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `ID_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
